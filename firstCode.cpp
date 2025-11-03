3ceee8de3f16cea8b3805b28cd5c5975b59541a3
Shah Jalal Dip <shahjalaldip246@gmail.com>
2025-11-03 15:43:54 +0600

    init

---
+++ firstCode.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define inout() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+#define endl '\n'
+#define ll long long
+#define int long long
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sq(a) ((a) * (a))
+#define subStr(s,p) s.find(p)!=s.npos
+#define forloop(i,n) for(int xx=i;xx<n;xx++)
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const int MX = 2e5+123;
+const ll infLL = 9000000000000000000;
+const int MOD = 1e9+7;
+int32_t main(){
+       int t;
+       cin>>t;
+       while
+return 0;
+}
